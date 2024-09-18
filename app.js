eedc9eee71642bc48db28af4dcb98c23e9306f4b
gasperpb <achillespb@hotmail.com>
2024-09-18 17:04:41 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,343 @@
+```json
+{
+  "Prefeitura Municipal de São
+Gonçalo do Rio Abaixo/MG 2016": [
+    {
+      "pergunta": "As feridas podem ser classificadas de três formas diferentes: de acordo com a maneira como foram produzidas, de acordo com o grau de contaminação e, ainda, com o comprometimento tecidual[^1^][1]. Nesse sentido, assinale a afirmativa correta.",[^2^][2]
+      "alternativas": [
+        "A) As feridas perfurantes são caracterizadas por grande abertura na pele.",[^3^][3]
+        "B) As feridas contusas são aquelas com margens irregulares como as produzidas por vidro ou arame farpado.",[^4^][4]
+        "C) As feridas laceradas são produzidas por objeto rombo e são caracterizadas por traumatismo das partes moles, hemorragia e edema.",[^5^][5]
+        "D) As feridas incisas ou cirúrgicas são aquelas produzidas por um instrumento cortante[^6^][6]. As feridas limpas geralmente são fechadas por suturas."[^7^][7]
+      ]
+    },
+    {
+      "pergunta": "A administração de medicamentos por via ______________ é segura e não requer técnica estéril na sua preparação; nessa via os medicamentos podem ser na apresentação de ______________ que são absorvidos, principalmente, no estômago e intestino[^8^][8]. Assinale a alternativa que completa correta e sequencialmente a afirmativa anterior.",[^9^][9]
+      "alternativas": [
+        "A) oral / comprimidos",
+        "B) injetável / ampolas",
+        "C) endovenosa / ampolas",
+        "D) intramuscular / cápsulas"
+      ]
+    },
+    {
+      "pergunta": "A cesariana é uma forma de parto realizada através de um ato cirúrgico, no qual é feita uma incisão no abdômen e outra no útero para se chegar ao bebê[^10^][10]. Um parto cesário dura em média 45 minutos a uma hora[^11^][11]. São situações médicas que habitualmente indicam a programação prévia de uma cesariana, EXCETO:",[^12^][12]
+      "alternativas": [
+        "A) Gravidez gemelar.",[^13^][13]
+        "B) Gravidez na adolescência.",[^14^][14]
+        "C) Quando o bebê está na posição errada, de lado ou com a cabeça para cima.",[^15^][15]
+        "D) Quando a placenta está implantada de forma anormal, como nos casos de placenta prévia."
+      ]
+    },
+    {
+      "pergunta": "A gripe H1N1 consiste em uma doença causada por uma mutação do vírus da gripe[^16^][16]. Em 2016 a gripe H1N1 chegou mais cedo ao Brasil[^17^][17]. Em março deste ano o número de casos só no estado de São Paulo superou a quantidade de pessoas doentes em 2015 em todo o país[^18^][18]. Em 2016 uma nova vacina da gripe será lançada na campanha nacional de vacinação contra a gripe, e ela também contemplará a H1N1[^19^][19]. A vacinação normalmente é oferecida na rede pública para pessoas dentro dos grupos de risco[^20^][20]. São considerados grupos de risco, EXCETO:",[^21^][21]
+      "alternativas": [
+        "A) Gestantes.",[^22^][22]
+        "B) Idosos acima de 60 anos.",[^23^][23]
+        "C) Crianças entre seis meses e cinco anos.",[^24^][24]
+        "D) Portadores de doenças crônicas, como diabetes e hipertensão arterial."[^25^][25]
+      ]
+    },
+    {
+      "pergunta": "A febre Chikungunya é uma doença transmitida pelos mosquitos Aedes aegypti e Aedes albopictus[^26^][26]. No Brasil, a circulação do vírus foi identificada pela primeira vez em 2014[^27^][27]. Em relação à febre Chikungunya, assinale a afirmativa INCORRETA.",[^28^][28]
+      "alternativas": [
+        "A) É possível ter chikungunya mais de uma vez.",[^29^][29]
+        "B) Os sintomas iniciam entre dois e doze dias após a picada do mosquito.",[^30^][30]
+        "C) Os principais sintomas são: febre alta de início rápido, dores intensas nas articulações dos pés e mãos, além de dedos, tornozelos e pulsos.",[^31^][31]
+        "D) Chikungunya significa “aqueles que se dobram” em swahili, um dos idiomas da Tanzânia[^32^][32]. Refere-se à aparência curvada dos pacientes que foram atendidos na primeira epidemia documentada, na Tanzânia."[^33^][33]
+      ]
+    },
+    {
+      "pergunta": "Em relação à Legislação do Sistema Único de Saúde, assinale a afirmativa INCORRETA.",[^34^][34]
+      "alternativas": [
+        "A) As instituições privadas poderão participar de forma complementar do Sistema Único de Saúde, segundo diretrizes deste, mediante contrato de direito público ou convênio, exclusivo apenas às entidades filantrópicas e as sem fins lucrativos.",[^35^][35]
+        "B) A saúde é direito de todos e dever do Estado, garantido mediante políticas sociais e econômicas que visem à redução do risco de doença e de outros agravos e ao acesso universal e igualitário às ações e serviços para sua promoção, proteção e recuperação.",[^36^][36]
+        "C) A lei disporá sobre as condições e os requisitos que facilitem a remoção de órgãos, tecidos e substâncias humanas para fins de transplante, pesquisa e tratamento, bem como coleta, processamento e transfusão de sangue e seus derivados, sendo vedado todo tipo de comercialização.",[^37^][37]
+        "D) São de relevância pública as ações e serviços de saúde, cabendo ao poder público dispor, nos termos da lei, sobre sua regulamentação, fiscalização e controle, devendo sua execução ser feita diretamente ou através de terceiros e, também, por pessoa física ou jurídica de direito privado."[^38^][38]
+      ]
+    },
+    {
+      "pergunta": "A saúde tem como fatores determinantes e condicionantes, entre outros, a alimentação, a moradia, o saneamento básico, o meio ambiente, o trabalho, a renda, a educação, o transporte, o lazer e o acesso aos bens e serviços essenciais; os níveis de saúde da população expressam a organização social e econômica do País[^39^][39]. As ações dispostas anteriormente, se destinam a garantir às pessoas e à coletividade condições de bem-estar",[^40^][40]
+      "alternativas": [
+        "A) físico, mental e social.",[^41^][41]
+        "B) físico, ambiental e social.",[^43^][43]
+        "C) físico, financeiro e mental.",[^42^][42][^41^][41]
+        "D) social, financeiro e ambiental."[^43^][43]
+      ]
+    },
+    {
+      "pergunta": "O tratamento recomendado para os casos sintomáticos de infecção pelo vírus zika é baseado no uso de acetaminofeno (paracetamol) ou dipirona para o controle da febre e da dor[^44^][44]. No caso de erupções pruriginosas, anti-histamínico pode ser prescrito[^45^][45]. NÃO se recomenda o uso de ácido acetilsalicílico e outros anti-inflamatórios, em função do risco aumentado de complicações",[^46^][46]
+      "alternativas": [
+        "A) motoras.",[^47^][47]
+        "B) intestinais.",
+        "C) neurológicas.",[^48^][48]
+        "D) hemorrágicas."
+      ]
+    },
+    {
+      "pergunta": "No Brasil, cerca de sete milhões de pessoas, acima de 18 anos, têm a doença[^49^][49]. Um estudo recente da Sociedade Brasileira de Diabetes aponta que mais de 60% deles não sabem que têm a doença[^50^][50]. Diabetes é causada pela disfunção metabólica crônica decorrente de uma deficiência de insulina – hormônio produzido pelo",[^51^][51]
+      "alternativas": [
+        "A) baço.",
+        "B) útero.",[^52^][52]
+        "C) fígado.",[^53^][53]
+        "D) pâncreas."[^54^][54]
+      ]
+    },
+    {
+      "pergunta": "Pneumonias são provocadas pela penetração de um agente infeccioso ou irritante (bactérias, vírus, fungos e por reações alérgicas) no espaço alveolar, onde ocorre a troca gasosa[^55^][55]. Esse local deve estar sempre muito limpo, livre de substâncias que possam impedir o contato do ar com o sangue[^56^][56]. Provocam pneumonia, EXCETO:",[^57^][57]
+      "alternativas": [
+        "A) Vírus.",[^58^][58]
+        "B) Fungos.",[^59^][59]
+        "C) Refluxo.",[^60^][60]
+        "D) Bactéria."
+      ]
+    }
+  ]
+}
+```
+{
+    "pergunta": "Quanto à idade da população podemos considerar “idoso”, segundo a Assembleia Mundial sobre Envelhecimento (Viena-Áustria, 1982), aquele que:",
+    "alternativas": [
+        "A) Possui 60 anos ou mais.",
+        "B) Possui 65 anos ou mais.",
+        "C) Possui 70 anos ou mais.",
+        "D) Apresenta doenças crônicas."
+    ]
+},
+{
+    "pergunta": "A icterícia é uma ocorrência comum no período neonatal. Dois entre três recém-nascidos a termo e uma porcentagem ainda maior em prematuros desenvolvem icterícia clínica. Dentre as condutas estão a alimentação precoce e a fototerapia. Os cuidados de enfermagem para um recém-nascido em fototerapia incluem, EXCETO:",
+    "alternativas": [
+        "A) Pesar uma vez ao dia.",
+        "B) Controlar temperatura.",
+        "C) Utilizar protetores oculares.",
+        "D) Dosar bilirrubina somente na alta da criança."
+    ]
+},
+{
+    "pergunta": "Recomenda-se que um sistema de precauções padrão ou universal seja adotado por todos os profissionais de saúde envolvidos na assistência aos pacientes atendidos em instituições de saúde. Todos os pacientes, mesmo não apresentando sintomas específicos, devem ser considerados potenciais portadores de doenças transmissíveis e, portanto, o profissional deverá adotar as seguintes medidas:",
+    "alternativas": [
+        "A) Lavagem das mãos e uso de óculos de proteção.",
+        "B) Lavagem das mãos e uso de luvas de procedimento.",
+        "C) Uso de luvas de procedimento e óculos de proteção.",
+        "D) Lavagem das mãos, uso de luvas de procedimento e óculos de proteção."
+    ]
+},
+{
+    "pergunta": "As feridas, qualquer lesão que leve à descontinuidade da pele, são classificadas quanto à causa, à etiologia e ao conteúdo microbiano. As feridas consideradas contaminadas são:",
+    "alternativas": [
+        "A) Com rupturas de vasos, incisas por cortes, laceração e perfuração.",
+        "B) Lesões feitas em condições assépticas e isentas de micro-organismos.",
+        "C) Lesões com tempo inferior a seis horas entre o trauma e o atendimento e sem contaminação significativa.",
+        "D) Lesões com tempo superior a seis horas entre o trauma e o atendimento e com presença de contaminantes, mas sem processo infeccioso."
+    ]
+}
+```json
+{
+  "prova": {
+    "cargo": "Técnico em Enfermagem",[^3^][3]
+    "data": "08/01/2017",
+    "organizadora": "Prefeitura Municipal de São Gonçalo do Rio Abaixo/MG",
+    "questoes": [
+      {
+        "numero": 1,
+        "enunciado": "Segundo as informações trazidas no texto, assinale a afirmativa INCORRETA.",[^4^][4]
+        "alternativas": [
+          "A) A desorganização familiar contribui para a inserção do jovem no mundo do crime.",[^6^][6]
+          "B) O suicídio e os acidentes automobilísticos também estão ligados à morte de jovens brasileiros.",[^7^][7]
+          "C) No Brasil, as mortes de jovens ligadas à violência são superiores em relação aos países desenvolvidos.",[^8^][8]
+          "D) Apesar de cometerem o crime no aeroporto, os jovens temiam as autoridades, como se comprovou através das imagens das câmeras."[^9^][9]
+        ]
+      },
+      {
+        "numero": 2,
+        "enunciado": "Assinale a alternativa que justifica o uso das vírgulas no trecho anterior.",
+        "alternativas": [
+          "A) Separam orações intercaladas.",[^10^][10]
+          "B) Assinalam a inversão de adjuntos adverbiais.",[^11^][11]
+          "C) Separam termos com a mesma função sintática.",[^12^][12]
+          "D) Marcam o deslocamento do complemento do verbo."[^13^][13]
+        ]
+      },
+      {
+        "numero": 3,
+        "enunciado": "A expressão sublinhada, além de introduzir o período, expressa a ideia de:",[^14^][14]
+        "alternativas": [
+          "A) Exclusão.",
+          "B) Adversidade.",[^15^][15]
+          "C) Fechamento.",[^16^][16]
+          "D) Justificativa; razão."[^17^][17]
+        ]
+      },
+      {
+        "numero": 4,
+        "enunciado": "Assinale a alternativa cuja palavra sublinhada apresenta significado correto.",[^18^][18]
+        "alternativas": [
+          "A) direção",
+          "B) conjectura",
+          "C) comumente",
+          "D) originado"
+        ]
+      },
+      {
+        "numero": 5,
+        "enunciado": "Assinale a alternativa que apresenta o referente indicado INCORRETAMENTE.",[^19^][19]
+        "alternativas": [
+          "A) Soares",
+          "B) Marlon Roldão Soares",
+          "C) garotos",
+          "D) disputa de duas facções criminosas rivais"[^20^][20]
+        ]
+      },
+      {
+        "numero": 6,
+        "enunciado": "Quanto ao emprego do verbo “poderia”, neste contexto, assinale a afirmativa correta.",
+        "alternativas": [
+          "A) Expressa a causa de um fato ocorrido anteriormente ao atual.",[^21^][21]
+          "B) Indica a possibilidade da causa de uma ação ocorrida no passado.",[^23^][23][^24^][24]
+          "C) Apresenta a causa de uma ação situada de forma incerta no tempo passado.",
+          "D) Indica a causa de um fato ocorrido no passado que se estende até o presente."[^22^][22]
+        ]
+      },
+      {
+        "numero": 7,
+        "enunciado": "Quanto à análise sintática, assinale a alternativa cujo termo ou trecho sublinhado se DIFERE dos demais.",[^25^][25]
+        "alternativas": [
+          "A) Até a quarta-feira, dia 21, não estava clara a causa do assassinato,...",[^26^][26][^27^][27]
+          "B) É o retrato de uma guerra urbana, que provoca a morte de dezenas de jovens,...",
+          "C) O jovem não tinha antecedentes criminais e não parecia ter relação com o tráfico.",
+          "D) A cena dos garotos saindo do aeroporto, rosto limpo, dando tiros para o alto, pegando ‘carona’ em um carro que os aguardava, sem se preocupar se estavam sendo gravados, revela um desprezo com as autoridades."[^28^][28][^29^][29]
+        ]
+      },
+      {
+        "numero": 8,
+        "enunciado": "Quanto à classe de palavras, os termos anteriormente sublinhados classificam-se, dentro do contexto empregado, respectivamente, como:",
+        "alternativas": [
+          "A) Interjeição, pronome e conjunção.",[^30^][30][^31^][31]
+          "B) Preposição, pronome e conjunção.",[^32^][32][^33^][33]
+          "C) Interjeição, conjunção e pronome.",[^31^][31][^30^][30]
+          "D) Preposição, conjunção e pronome."[^33^][33][^32^][32]
+        ]
+      },
+      {
+        "numero": 9,
+        "enunciado": "Os dois-pontos foram utilizados para anunciar",
+        "alternativas": [
+          "A) um aposto.",[^34^][34]
+          "B) um exemplo.",[^35^][35]
+          "C) uma enumeração.",[^36^][36]
+          "D) uma informação relacionada ao que foi mencionado anteriormente."[^37^][37]
+        ]
+      },
+      {
+        "numero": 10,
+        "enunciado": "De acordo com o contexto empregado, assinale a alternativa que substitui corretamente o termo sublinhado.",[^38^][38]
+        "alternativas": [
+          "A) E.",
+          "B) Assim.",[^39^][39]
+          "C) Portanto.",[^40^][40]
+          "D) Entretanto."[^41^][41]
+        ]
+      },
+      {
+        "numero": 11,
+        "enunciado": "O acesso aos serviços de saúde em todos os níveis de assistência é um dos princípios do Sistema Único de Saúde (SUS)[^42^][42]. Esse princípio é denominado:",[^43^][43]
+        "alternativas": [
+          "A) Social.",[^44^][44]
+          "B) Integralidade.",[^45^][45]
+          "C) Descentralização.",[^46^][46]
+          "D) Regionalização do sistema."[^47^][47]
+        ]
+      },
+      {
+        "numero": 12,
+        "enunciado": "Acerca dos coeficientes que medem taxas associadas a óbitos, analise as alternativas a seguir.",[^48^][48]
+        "alternativas": [
+          "A) I, II, III e IV.",[^49^][49][^50^][50]
+          "B) IV, apenas.",[^51^][51][^55^][55]
+          "C) I e II, apenas.",[^53^][53][^52^][52]
+          "D) III e IV, apenas."[^55^][55][^51^][51]
+        ]
+      },
+      {
+        "numero": 13,
+        "enunciado": "Entre as doenças a seguir NÃO há a necessidade de notificação compulsória junto aos serviços de vigilâncias em saúde, apenas em:",[^56^][56]
+        "alternativas": [
+          "A) Sífilis.",[^57^][57]
+          "B) HIV/Aids.",[^58^][58]
+          "C) Toxoplasmose.",[^59^][59]
+          "D) Leishmaniose visceral."[^60^][60]
+        ]
+      },
+      {
+        "numero": 14,
+        "enunciado": "Uma medida comum de prevenção primária (proteção específica) para ambas as doenças é aumentar",[^61^][61]
+        "alternativas": [
+          "A) a quimioprofilaxia junto à população.",
+          "B) a distribuição de água potável na cidade.",[^62^][62]
+          "C) a cobertura vacinal entre recém-nascidos.",[^63^][63]
+          "D) os serviços de vigilância sanitária de alimentos."[^64^][64]
+        ]
+      },
+      {
+        "numero": 15,
+        "enunciado": "São atribuições dos profissionais de saúde pública que atuam especificadamente junto aos programas de Estratégia em Saúde da Família (ESF), ampliando um dos principais programas de saúde no Brasil:",[^65^][65]
+        "alternativas": [
+          "A) I, II e III.",[^50^][50][^49^][49]
+          "B) I, apenas.",[^52^][52][^53^][53]
+          "C) I e II, apenas.",
+          "D) II e III, apenas."[^54^][54][^55^][55]
+        ]
+      },
+      {
+        "numero": 16,
+        "enunciado": "Qual dos conjuntos a seguir é finito?",[^66^][66]
+        "alternativas": [
+          "A) Conjunto dos números reais menores que –5.",[^67^][67]
+          "B) Conjunto dos números inteiros maiores que –2.",[^68^][68]
+          "C) Conjunto dos números naturais menores que 7.",[^69^][69]
+          "D) Conjunto dos números racionais menores que 4."[^67^][67]
+        ]
+      },
+      {
+        "numero": 17,
+        "enunciado": "Marcela comprou três livros sendo: um dicionário, um almanaque e um atlas[^70^][70]. O dicionário custou R$ 25,00 a mais que o atlas e R$ 11,00 a menos que o almanaque[^71^][71]. Se no total os três custaram R$ 225,00, quanto custou o livro mais barato?",[^72^][72]
+        "alternativas": [
+          "A) R$ 58,00.",[^73^][73]
+          "B) R$ 62,00.",[^74^][74]
+          "C) R$ 76,00.",[^75^][75]
+          "D) R$ 83,00."[^76^][76]
+        ]
+      },
+      {
+        "numero": 18,
+        "enunciado": "A sequência a seguir é uma progressão geométrica: 4x + 80, 3x – 100, x – 40, ?[^77^][77] Se a soma dos três primeiros termos dessa sequência é igual a 420, então o número que substitui corretamente a interrogação é:",[^78^][78]
+        "alternativas": [
+          "A) 4.",[^1^][1][^79^][79]
+          "B) 5.",[^2^][2]
+          "C) 6.",[^5^][5]
+          "D) 7."[^80^][80]
+        ]
+      },
+      {
+        "numero": 19,
+        "enunciado": "A soma dos elementos do conjunto [(A ∩ B)] ∪ [(B ∪ C) ∩ (C ∩ A)] é igual a:",[^81^][81]
+        "alternativas": [
+          "A) 22.",[^82^][82]
+          "B) 26.",[^83^][83]
+          "C) 29.",[^84^][84]
+          "D) 35."[^85^][85]
+        ]
+      },
+      {
+        "numero": 20,
+        "enunciado": "O maior valor inteiro de m para o qual a equação x² – 4x – m = 0 NÃO apresenta raízes reais é:",[^86^][86][^87^][87]
+        "alternativas": [   
+            "A) 3.",[^88^][88]
+            "B) 4.",[^89^][89]
+            "C) 5.",[^90^][90]
+            "D) 6."[^91^][91]
+            ]                               
+        }
+    ]
+    }
+}
